f9e42ed225523d676b5c873073c2a4fdc2f651b4
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-08-04 14:23:09 -0700

    small changes

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search
+
+"""
+UPER:
+U:
+    > 
+
+"""
